49241b7041bd1577226d2b6967c8e2668af5a9dc
Nduka Oweniwe <oweniwenduka@gmail.com>
2026-02-10 12:38:38 +0100

    Made definition to display write up gradually at the front end(script.js file)

--- public/scripts.js
+++ public/scripts.js
@@ -2,6 +2,13 @@ const input = document.getElementById("search");
 const button = document.getElementById("BTN");
 const resultDiv = document.getElementById("result");
 
+async function typeText(element, text, delay = 50){
+    element.textContent = "";//clear previous content
+    for (let i = 0; i < text.length; i++){
+        element.textContent += text[i];
+        await new Promise(resolve => setTimeout(resolve, delay));
+    }
+}
 button.addEventListener("click", async()=>{
     const word = input.value.trim().toLowerCase();
     if(!word){
@@ -17,10 +24,12 @@ button.addEventListener("click", async()=>{
         if (response.status ===404){
             resultDiv.textContent = `"${word}" not found in the dictionary.`;
         } else{
-            resultDiv.textContent = `${data.word}: ${data.definition}`;
+            // resultDiv.textContent = `${data.word}: ${data.definition}`;
+            await typeText(resultDiv, `${data.word}: ${data.definition}`, 30);
         }
     } catch(err){
         resultDiv.textContent = "Error connecting to the server."
         console.error(err)
     }
+
 })
